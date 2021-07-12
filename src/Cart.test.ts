7e83c731e10333a84af720c91ca517123a9e5030
Elias Binchamo <eliasbinchamo@gmail.com>
2021-07-12 17:11:15 -0400

    added add to cart test and implementation

--- src/Cart.test.ts
+++ src/Cart.test.ts
@@ -7,3 +7,57 @@ describe('Cart', () => {
         expect(cart.totalPrice).toEqual(0);
     })
 });
+// Given I have an empty cart, when I add an Item, 
+// then I expect the price to reflect the sum of all the Items in my cart.
+describe('Cart', () => {
+    test('should add items to the cart', () => {
+        const cart = new Cart();
+        cart.addItem("Soda",12);
+
+        expect(cart.items).toEqual([{"itemName": "Soda", "itemPrice": 12}]);
+        expect(cart.totalPrice).toEqual(12);
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('Add Item', () => {
+//     test('Should add an Item to the cart', () => {
+//         const cart2 = new Cart();
+//         cart2.addItem("Milk", 32);
+//         expect(cart2.items).toEqual(["Milk"]);
+//         expect(cart2.totalPrice).toEqual(32);
+//     })
+// });
+
+// describe('List Items', () => {
+//     test('Should list Items in the cart with prices', () => {
+//         const cart2 = new Cart();
+//         cart2.addItem("Milk", 32);
+//         cart2.addItem("Water", 32);
+//         expect(cart2.items).toEqual(["Milk","Water"]);
+//         expect(cart2.totalPrice).toEqual(128);
+//         cart2.printList();
+//     })
+// });
+
--- src/Cart.ts
+++ src/Cart.ts
@@ -1,2 +1,39 @@
 export default class Cart {
+    items: Array<Object> = [];
+    totalPrice: number = 0;
+    itemMap: Map<string,number> = new Map;
+
+    addItem(itemName: string, itemPrice: number) {
+        try {
+            let dictionary = {
+                "itemName": itemName,
+                "itemPrice": itemPrice
+            }
+            this.items.push(dictionary);
+            this.totalPrice+= itemPrice;
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+
+
+
+
+    // items: Array<string> = [];
+    // totalPrice = 0;
+    // itemsWithPrices: Map<string,number>=new Map;
+    // itemList: Array<string> = [];
+    // addItem(itemName: string, itemPrice:number) {
+    //     this.items.push(itemName);
+    //     this.totalPrice+=itemPrice;
+    //     this.itemsWithPrices.set(itemName,itemPrice);
+    // }
+
+    // printList(){
+    //     this.itemsWithPrices.forEach((price, item)=> {
+    //         this.itemList.push("[item:"+item+", Price:"+price+"]")
+    //     });
+    // }
+    
 }
